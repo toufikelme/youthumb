09ddd2a1b1c53ad5214176845d221690437c3d6b
toufik <toufikelmessaoudi8@gmail.com>
2023-09-07 19:08:32 +0100

    ret

--- pages/index.js
+++ pages/index.js
@@ -201,6 +201,7 @@ const Index = () => {
             src="https://gourmetads.com/wp-content/uploads/2019/02/160x600-nespresso.jpg"
             className="img-responsive wp-image-2053"
           />
+<img decoding="async" fetchpriority="high" width="160" height="600" alt="160x600 Skyscraper Ad Unit" title="160×600 Nespresso" src="https://gourmetads.com/wp-content/uploads/2019/02/160x600-nespresso.jpg" class="img-responsive wp-image-2053"></img>
         </div>
       </div>	  
     </div>
